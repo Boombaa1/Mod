dfb938c32c00179969ee44f1aa27e1d9d3c158c7
Boombaa1 <timafurman78@gmail.com>
2026-05-26 16:34:55 +0300

    Create FastXPMod.java

---
+++ src/main/java/com/example/fastxp/FastXPMod.java
@@ -0,0 +1,10 @@
+package com.example.fastxp;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("fastxp")
+public class FastXPMod {
+    public FastXPMod() {
+        // Главный класс мода для Forge
+    }
+}
