3605625dec49a204f1d5c9e7ac43c3a056af2678
Boombaa1 <timafurman78@gmail.com>
2026-05-27 17:05:51 +0300

    Update FastXPMixin.java

--- src/main/java/com/example/fastxp/mixin/FastXPMixin.java
+++ src/main/java/com/example/fastxp/mixin/FastXPMixin.java
@@ -4,12 +4,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.item.ExperienceBottleItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class FastXPMixin {
+
+    @Shadow
+    private int rightClickDelay;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void fastXP(CallbackInfo ci) {
@@ -20,10 +24,10 @@ public class FastXPMixin {
 
         if (player == null) return;
 
-        // FIX:
-        // теперь работает только для XP
+        // FastXP только для XP бутылок
         if (player.getMainHandItem().getItem() instanceof ExperienceBottleItem) {
-            mc.rightClickDelay = 0;
+
+            this.rightClickDelay = 0;
         }
     }
 }
